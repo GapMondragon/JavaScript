651fb8f855474ebc54664bac5774bd1bea97e8b9
Gabriel Paolo Mondragon <mondragon.gabrielpaolo@gmail.com>
2022-12-14 09:08:15 +0800

    literals

---
+++ js-basics/index.js
@@ -0,0 +1,13 @@
+// JavaScript Review!!! 
+// console.log('Hello byeeeeeee');
+// https://youtu.be/W6NZfCO5SIk
+
+let firstName = "Gabriel Paolo";
+let lastName = "Mondragon";
+console.log(firstName + " " + lastName);
+
+let name = "Gabe"; // string literal
+let age = 27; // number literal 
+let isApproved = false; // boolean literal
+let pet = undefined;
+let selectedColor = null;
